d419b032d171ce06be262552dda68cc42c50e853
Jacob Habib <habib.50@osu.edu>
2021-01-12 22:09:49 -0500

    Eliminated unecessary logo and added config js

---
+++ tailwind.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+    purge: [
+        './components/Navbar.vue',
+        './layouts/default.vue',
+        './pages/about.vue',
+        './pages/blog.vue',
+        './pages/contact.vue',
+        './pages/index.vue',
+        './pages/projects.vue'
+    ],
+    darkMode: false, // or 'media' or 'class'
+    theme: {
+      extend: {},
+    },
+    variants: {
+      extend: {},
+    },
+    plugins: [],
+  }
