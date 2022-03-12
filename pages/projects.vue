17d1ddb5caa86cefc409cebfe50feaf5a2107a39
Jacob Habib <habib.50@osu.edu>
2022-03-11 23:20:55 -0500

    added web3 projects

--- pages/projects.vue
+++ pages/projects.vue
@@ -7,22 +7,76 @@
     </div>
     <div class="md:h-full h-screen w-screen bg-gradient-to-br from-blue-100 via-gray-200 to-blue-200 overflow-visible">
       <h1 class="flex text-2xl md:text-3xl pt-4 font-black justify-center"> Projects </h1>
+      <h1 class="flex text-2xl md:text-3xl pt-4 font-black justify-center">(Newest to Oldest)</h1>
+      <p
+        class="flex text-1xl md:text-2xl pt-4 md:pt-8 justify-center text-center text-blue-700 font-black">
+        Chainshot Projects (Web3)</p>
+      <p class="flex text-1xl md:text-2xl justify-center text-center"><a
+        href="https://github.com/jahabeebs/QR-Culture" class="hover:text-blue-700 italic">
+        ETH Global Hackathon & Chainshot Final Project: NFT Rewards Program </a></p>
+      <p class="flex text-1xl md:text-2xl justify-center text-center"><a
+        href="https://github.com/jahabeebs/Chainlink-Practice" class="hover:text-blue-700 italic">
+        Chainlink VFR & Price Feeds Experiment</a></p>
+      <p class="flex text-1xl md:text-2xl justify-center text-center"><a
+        href="https://github.com/jahabeebs/Upgradeable-Open-Zepellin-Experiment" class="hover:text-blue-700 italic">
+        Open Zeppelin Upgradeable Contract Experiment </a>
+      </p>
+      <p class="flex text-1xl md:text-2xl justify-center text-center"><a
+        href="https://github.com/jahabeebs/Airdrop-Contract" class="hover:text-blue-700 italic"> Token Airdrop </a></p>
+      <p class="flex text-1xl md:text-2xl justify-center text-center"><a
+        href="https://github.com/jahabeebs/simple-erc20"
+        class="hover:text-blue-700 italic"> ERC20 </a></p>
+      <p class="flex text-1xl md:text-2xl justify-center text-center"><a
+        href="https://github.com/jahabeebs/ethblockexplorer" class="hover:text-blue-700 italic"> ETH Block Explorer </a>
+      </p>
+      <p class="flex text-1xl md:text-2xl justify-center text-center"><a
+        href="https://github.com/jahabeebs/Escrow-Hardhat"
+        class="hover:text-blue-700 italic"> Escrow App </a></p>
+      <p class="flex text-1xl md:text-2xl justify-center text-center"><a
+        href="https://github.com/jahabeebs/RSA-Encryption" class="hover:text-blue-700 italic"> RSA Encryption
+        Basics </a></p>
+      <p class="flex text-1xl md:text-2xl justify-center text-center"><a
+        href="https://github.com/jahabeebs/Call-And-Delegate-Contract-Calls"
+        class="hover:text-blue-700 italic"> Call & Delegate Call Practice </a></p>
+      <p class="flex text-1xl md:text-2xl justify-center text-center"><a
+        href="https://github.com/jahabeebs/ECDSA-Crypto-Exchange" class="hover:text-blue-700 italic"> ECDSA Crypto
+        Exchange </a></p>
+      <p class="flex text-1xl md:text-2xl justify-center text-center"><a
+        href="https://github.com/jahabeebs/ECDSA-Signatures"
+        class="hover:text-blue-700 italic"> ECDSA Encryption Basics </a></p>
+      <p class="flex text-1xl md:text-2xl justify-center text-center"><a
+        href="https://github.com/jahabeebs/contract-puzzles"
+        class="hover:text-blue-700 italic"> Contract Testing w/Hardhat </a></p>
+      <p
+        class="flex text-1xl md:text-2xl pt-4 md:pt-8 justify-center text-center text-blue-700 font-black hover:text-blue-900 italic">
+        <a href="https://herodao-ipvzjb00n-jahabeebs.vercel.app/"> Landing Page for HeroDao (Web3)</a></p>
+      <p class="flex text-1xl md:text-2xl justify-center text-center"> Technologies Used:</p>
+      <p class="flex text-1xl md:text-2xl justify-center text-center"><a href="https://www.nuxtjs.org"
+                                                                         class="hover:text-blue-700 italic"> Nuxt.JS&nbsp;//</a>
+        <a href="https://www.tailwindcss.com" class="hover:text-blue-700 italic">&nbsp;Tailwind CSS&nbsp;//</a> <a
+          href="https://vercel.com/" class="hover:text-blue-700 italic">&nbsp;Vercel // </a><a
+          href="https://www.figma.com/" class="hover:text-blue-700 italic"> &nbsp;Figma </a></p>
       <p
         class="flex text-1xl md:text-2xl pt-4 md:pt-8 justify-center text-center text-blue-700 font-black hover:text-blue-900 italic">
         <a href="https://cmarketprice.com"> Commodity Market Prices</a></p>
-      <p class="flex text-1xl md:text-2xl justify-center italic text-center"> Winner of Cloudflare Summer Developer Challenge</p>
+      <p class="flex text-1xl md:text-2xl justify-center italic text-center"> Winner of Cloudflare Summer Developer
+        Challenge</p>
       <p class="flex text-1xl md:text-2xl justify-center text-center"> Technologies Used:</p>
-      <p class="flex text-1xl md:text-2xl justify-center text-center"><a href="https://www.typescriptlang.org/" class="hover:text-blue-900 italic"> Typescript //</a>
-       <a href="https://pages.cloudflare.com/" class="hover:text-blue-700 italic">&nbsp;Cloudflare Pages</a></p>
-     <p class="flex text-1xl md:text-2xl justify-center text-center">
-      <a
-        href="https://www.chartjs.org/" class="hover:text-blue-700 italic">&nbspChart.js // </a> <a
-      href="https://workers.cloudflare.com/" class="hover:text-blue-700 italic">&nbsp;Cloudflare Workers</a>
+      <p class="flex text-1xl md:text-2xl justify-center text-center"><a href="https://www.typescriptlang.org/"
+                                                                         class="hover:text-blue-900 italic"> Typescript
+        //</a>
+        <a href="https://pages.cloudflare.com/" class="hover:text-blue-700 italic">&nbsp;Cloudflare Pages</a></p>
+      <p class="flex text-1xl md:text-2xl justify-center text-center">
+        <a
+          href="https://www.chartjs.org/" class="hover:text-blue-700 italic">&nbspChart.js // </a> <a
+        href="https://workers.cloudflare.com/" class="hover:text-blue-700 italic">&nbsp;Cloudflare Workers</a>
       </p>
       <p class="flex text-1xl md:text-2xl justify-center text-center">
         <a
-          href="https://www.cloudflare.com/products/workers-kv/" class="hover:text-blue-700 italic">&nbspWorkers KV // </a> <a
-        href="https://developers.cloudflare.com/workers/platform/cron-triggers" class="hover:text-blue-700 italic">&nbsp;Cron Triggers </a>
+          href="https://www.cloudflare.com/products/workers-kv/" class="hover:text-blue-700 italic">&nbspWorkers KV
+          // </a> <a
+        href="https://developers.cloudflare.com/workers/platform/cron-triggers" class="hover:text-blue-700 italic">&nbsp;Cron
+        Triggers </a>
       </p>
       <p
         class="flex text-1xl md:text-2xl pt-4 md:pt-8 justify-center text-center text-blue-700 font-black hover:text-blue-900 italic">
